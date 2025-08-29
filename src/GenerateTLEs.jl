8d8a4b1eb9c16a7349a5002841b5c610d176032b
Lucy Gavrich <gavrichlucy@gmail.com>
2025-08-29 15:56:47 -0400

    Resolve merge conflict

--- src/GenerateTLEs.jl
+++ src/GenerateTLEs.jl
@@ -17,7 +17,7 @@ end
 """
 Update the stored Starlink TLEs by querying Celestrak for new entries.
     Minimizes calls to Celestrak by only querying for Starlink satellites
-    that are listed in satcat.csv as active but do not appear in the 
+    that are listed in satcat.csv as active but do not appear in the
     local Starlink TLE database.
 
 TODO: remove inactive satellites
@@ -29,11 +29,9 @@ function update_starlink_tles(;verbose::Bool=false)
     existing_IDs = []
     if isfile(starlink_TLE_file_path)
         existing_TLEs = SatelliteToolboxTle.read_tles_from_file(starlink_TLE_file_path)
-        if verbose
-            println("Using $(length(existing_TLEs)) existing TLEs")
         existing_IDs = [tle.satellite_number for tle in existing_TLEs]
     end
-    save_TLEs(append!(existing_TLEs, get_active_satellite_TLEs(OBJECT_NAME="starlink", row_filter=row->!(parse(Int, row.NORAD_CAT_ID)∈existing_IDs), verbose=verbose)), starlink_TLE_file_path)
+    save_TLEs(get_active_satellite_TLEs(OBJECT_NAME="starlink", row_filter=row->!(parse(Int, row.NORAD_CAT_ID)∈existing_IDs), verbose=verbose), starlink_TLE_file_path)
 end
 
 """
