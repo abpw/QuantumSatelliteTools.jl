78cc75e2022d2992dc5c9cca2c9392be7429a6c1
connor <cacasey@umass.edu>
2026-03-04 16:26:05 -0500

    added a dr-watson based on the outline from the documentation

---
+++ scripts/intro.jl
@@ -0,0 +1,10 @@
+using DrWatson
+
+@quickactivate "QuantumSatelliteTools"
+
+println("Project name: ", projectname())
+println("Project dir: ", projectdir())
+println("Data dir: ", datadir())
+println("Scripts dir: ", scriptsdir())
+println("Plots dir: ", plotsdir())
+
