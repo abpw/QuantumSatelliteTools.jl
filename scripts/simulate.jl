20f70337ed4960ef7127c57e4489aa901482eec5
Lucy Gavrich <gavrichlucy@gmail.com>
2026-05-19 14:53:07 -0400

    More simulator stuff including updating evaluation_fn to work on Links rather than Channels

--- scripts/simulate.jl
+++ scripts/simulate.jl
@@ -1,7 +1,7 @@
 using Pkg
 Pkg.activate(joinpath(@__DIR__, ".."))
 
-using QuantumSatelliteTools.Simulator: build_optimized_constellation
+using QuantumSatelliteTools.Simulator: build_optimized_constellation, Node, NodeLabels, simulate
 using QuantumSatelliteTools.GenerateGroundStations: generate_grid_gses, generate_population_center_gses
 
 """
@@ -19,15 +19,38 @@ function main(;
     step_s::Int = 30,
 )
     # Build a constellation of 1000 satellites based on optimized parameters
-    propagators = build_optimized_constellation()
+    satellites = build_optimized_constellation()
 
     # Generate 100 population center ground stations.
-    population_centers = generate_population_center_gses(100)
+    population_center_gses = generate_population_center_gses(100)
 
     # Generate ground station grids with 200, 400, and 800 km spacing.
-    grid200 = generate_grid_gses(equatorial_distance_km=200)
-    grid400 = generate_grid_gses(equatorial_distance_km=400)
-    grid800 = generate_grid_gses(equatorial_distance_km=800)
+    grid_gses = Dict(
+        200 => generate_grid_gses(equatorial_distance_km=200),
+        400 => generate_grid_gses(equatorial_distance_km=400),
+        800 => generate_grid_gses(equatorial_distance_km=800),
+    )
 
+    # perform experiments for each grid scenario
+    for (spacing, aux_gses) in grid_gses
+        println("Simulating scenario with grid spacing of $spacing km...")
+
+        # Construct the nodes for this scenario
+        groups = [
+            (satellites, _ -> 0.0, [NodeLabels.sat]),
+            (population_center_gses, _ -> 1.0, [NodeLabels.gs]),
+            (aux_gses, _ -> 0.0, [NodeLabels.aux_gs]),
+        ]
+        nodes = [
+            Node(obj, weight_fn(obj), labels)
+            for (objs, weight_fn, labels) in groups
+            for obj in objs
+        ]
+
+        # Simulate the scenario and collect metrics
+        metrics = simulate(nodes, duration_s, step_s)
+
+        println("Metrics for grid spacing $spacing km: $metrics")
+    end
 
 end
--- src/Simulator.jl
+++ src/Simulator.jl
@@ -1,29 +1,67 @@
 using SatelliteToolboxPropagators: Propagators, OrbitPropagatorSgp4
 using ..GenerateSatellites: generate_regular_array_TLEs
+using ..FreespaceChannels: FreespaceChannel, AbstractChannel
 
+"""
+Enum identifying the role or type of a node in the quantum network.
+
+# Variants
+- `sat`: A satellite node.
+- `gs`: A primary ground station node.
+- `aux_gs`: An auxiliary ground station node.
+"""
 @enum NodeLabels begin
     sat
     gs
     aux_gs
 end
 
+"""
+Function for making an ID counter closure.
+"""
+function make_id_counter()
+    count = 0
+    return () -> (count += 1)
+end
+
+next_node_id = make_id_counter()
+next_link_id = make_id_counter()
+
+"""
+A node in the network graph representing either a propagator or ground station.
+
+# Fields
+- `id::Int`: Unique identifier for the node.
+- `obj::Union{Propagator, GroundStation}`: The underlying object.
+- `weight::Float64`: Weight associated with the node, used in graph algorithms.
+- `labels::Vector{NodeLabels}`: Collection of labels describing node properties.
+"""
 struct Node
     id::Int
     obj::Union{Propagator, GroundStation}
     weight::Float64
     labels::Vector{NodeLabels}
+
+    Node(obj, weight, labels) = new(next_node_id(), obj, weight, labels)
 end
 
-# TODO: write Channel to abstract Freespace/Fiber stuff
+"""
+A communication link between two nodes over a freespace channel.
 
+# Fields
+- `id::Int`: Unique identifier for the link.
+- `node1::Node`: One endpoint of the link.
+- `node2::Node`: The other endpoint of the link.
+- `channel:: AbstractChannel`: The model of the link's physical channel.
+"""
 struct Link
     id::Int
     node1::Node
     node2::Node
-    freespace_channel::FreespaceChannel
-end
+    channel::AbstractChannel
 
-# TODO: write pairwise function for downlink
+    Link(node1, node2, channel) = new(next_link_id(), node1, node2, channel)
+end
 
 """
 Build a synthetic constellation and initialize SGP4 propagators based on the
@@ -65,6 +103,29 @@ function build_optimized_constellation(;
 end
 
 """
+Dual downlink pairwise function that checks if one node is a satellite and the
+other is a ground station.
+
+# Arguments
+- `node1::Node`: First node.
+- `node2::Node`: Second node.
+
+# Returns
+- `Bool`: `true` if one node is a satellite and the other is a ground station,
+  `false` otherwise.
+"""
+function downlink_pairwise_fn(node1::Node, node2::Node; kwargs...)
+    if ((NodeLabels.sat in node1.labels && (NodeLabels.gs in node2.labels || NodeLabels.aux_gs in node2.labels))
+            || (NodeLabels.sat in node2.labels && (NodeLabels.gs in node1.labels || NodeLabels.aux_gs in node1.labels)))
+        channel = FreespaceChannel(node1.obj, node2.obj, kwargs...)
+        if !isnothing(channel)
+            return Link(node1, node2, channel)
+        end
+    end
+    return nothing
+end
+
+"""
 Perform a single time step of the simulation, applying the evaluation function.
 
 # Keyword Arguments
@@ -77,72 +138,64 @@ Perform a single time step of the simulation, applying the evaluation function.
 - The result of the evaluation function.
 """
 function simulate_step(;
-    nodes::Vector{Node},
-    PairwiseEvaluationFn::Function,
+    pairwise_fn::Function,
     evaluation_fn::Function,
-    time::Float64,
+    nodes::Vector{Node},
+    time::Float64;
     tx_aperture_m::Float64=0.6,
     rx_aperture_m::Float64=0.6,
     wavelength_nm::Float64=1550.0,
 )
-    # get freespace channels for all satellite-ground station pairs
-    channels = FreespaceChannel[]
-    for node in nodes
-        if NodeLabels.sat in node.labels
-            for gs_node in nodes
-                if NodeLabels.gs in gs_node.labels
-                    channel = FreespaceChannel(node.obj, gs_node.obj, transmitter_diameter_m=tx_aperture_m, receiver_diameter_m=rx_aperture_m, wavelength_nm=wavelength_nm, time=time)
-                    if !isnothing(channel)
-                        push!(channels, channel)
-                    end
-                end
+    # get links for all valid pairs of nodes
+    links = Link[]
+    for i in 1:length(nodes)
+        for j in (i+1):length(nodes)
+            link = pairwise_fn(
+                nodes[i],
+                nodes[j],
+                time=time,
+                transmitter_diameter_m=tx_aperture_m,
+                receiver_diameter_m=rx_aperture_m,
+                wavelength_nm=wavelength_nm
+            )
+            if !isnothing(link)
+                push!(links, link)
             end
         end
     end
 
     # apply the evaluation function to the current state of the channels
-    return evaluation_fn(channels)
+    return evaluation_fn(links)
 end
 
 """
 Simulate the constellation performance over a specified duration and time step.
 """
 function simulate(
-    propagators::Vector{Propagator},
-    gses::Vector{GroundStation},
-    aux_gses::Vector{GroundStation}=[],
+    nodes::Vector{Node},
     duration_s::Int,
     step_s::Int;
+    pairwise_fn::Function=downlink_pairwise_fn,
+    evaluation_fn::Function=default_evaluation_fn,
     tx_aperture_m::Float64=0.6,
     rx_aperture_m::Float64=0.6,
     wavelength_nm::Float64=1550.0,
 )
+    # collect metrics at each time step
+    metrics = []
+
     for t in 0:step_s:duration_s
-        # build nodes for this time step
-        # nodes = Node[]
-
-        # for (i, sat) in enumerate(propagators)
-        #     push!(nodes, Node(i, sat, 0, [NodeLabels.sat]))
-        # end
-
-        # for (i, gs) in enumerate(gses)
-        #     # TODO: assign weights based on population
-        #     push!(nodes, Node(length(propagators) + i, gs, 1.0, [NodeLabels.gs]))
-        # end
-
-        # for (i, aux_gs) in enumerate(aux_gses)
-        #     push!(nodes, Node(length(propagators) + length(gses) + i, aux_gs, 0, [NodeLabels.aux_gs]))
-        # end
-
-        # evaluate the current state of the constellation
-        metrics = simulate_step(
+        # evaluate the current state of the network
+        push!(metrics, simulate_step(
             nodes=nodes,
-            evaluation_fn=evaluate_constellation,
+            pairwise_fn=pairwise_fn,
+            evaluation_fn=evaluation_fn,
             time=t,
             tx_aperture_m=tx_aperture_m,
             rx_aperture_m=rx_aperture_m,
             wavelength_nm=wavelength_nm,
-        )
-
+        ))
     end
+
+    return metrics
 end
