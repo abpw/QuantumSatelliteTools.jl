3c9de176001c9e8feaf40073ff8b00c4ef5605f1
Lucy Gavrich <gavrichlucy@gmail.com>
2026-08-10 10:42:00 -0400

    Export VisualizeMap module and clean up styling

--- src/QuantumSatelliteTools.jl
+++ src/QuantumSatelliteTools.jl
@@ -1,14 +1,29 @@
 module QuantumSatelliteTools
 
-using GeoDatasets
-using SatelliteToolboxTle, SatelliteToolboxPropagators, SatelliteToolboxTransformations, SatelliteToolboxBase
-using SatelliteAnalysis
-using Downloads, CSV, Random, Dates, StaticArrays
+using CSV
+using Dates
+using Downloads
+using Random
+using StaticArrays
 
-export AstronomyGeometry, GenerateSatellites, GenerateGroundStations, FreespaceChannels, LossCalculation
+using GeoDatasets
+using SatelliteAnalysis
+using SatelliteToolboxBase
+using SatelliteToolboxPropagators
+using SatelliteToolboxTle
+using SatelliteToolboxTransformations
+
+export AstronomyGeometry
+export FreespaceChannels
+export GenerateGroundStations
+export GenerateSatellites
+export LossCalculation
+export Simulator
+export VisualizeMap
 
 module AstronomyGeometry
-export semimajor_radius, semiminor_radius, G, earth_mass_kg, seconds_per_day, equatorial_circumference_km, sin_60
+export semimajor_radius, semiminor_radius, G, earth_mass_kg, equatorial_circumference_km
+export seconds_per_day, sin_60
 export GS, norm, ellipsoid_line_intersection
 export geodetic_to_ecef, ecef_to_geodetic, eci_to_ecef, eci_to_geodetic
 export gs_gs_distance, sat_sat_distance
@@ -18,30 +33,49 @@ export Conditions, clear, fog, rain, snow
 include("AstronomyGeometry.jl")
 end
 
-module GenerateGroundStations
-export is_within_min_distance, is_land
-export generate_population_center_gses, _generate_population_center_gses, generate_city_gses, generate_random_gses, generate_equispaced_gses, generate_grid_gses
-include("GenerateGroundStations.jl")
-end
-
-module GenerateSatellites
-export refresh_satcat, update_starlink_TLEs, save_TLEs, get_active_satellite_TLEs, generate_regular_array_TLEs, get_active_satellites, generate_regular_array
-include("GenerateSatellites.jl")
-end
-
 module FreespaceChannels
 export FreespaceChannel
 include("FreespaceChannels.jl")
 end
 
+module GenerateGroundStations
+export is_within_min_distance, is_land
+export generate_population_center_gses, _generate_population_center_gses
+export generate_city_gses
+export generate_random_gses
+export generate_equispaced_gses
+export generate_grid_gses
+include("GenerateGroundStations.jl")
+end
+
+module GenerateSatellites
+export refresh_satcat, update_starlink_TLEs, save_TLEs
+export get_active_satellites, get_active_satellite_TLEs
+export generate_regular_array, generate_regular_array_TLEs
+include("GenerateSatellites.jl")
+end
+
 module LossCalculation
-export atmospheric_loss_dB, geometric_loss_dB, pointing_loss_dB, reflector_loss, swapping_loss, total_loss
+export dB_to_prob, prob_to_dB
+export atmospheric_loss_dB, geometric_loss_dB, pointing_loss_dB
+export reflector_loss
+export swapping_loss, swapping_loss_dB
+export total_loss, total_loss_dB
 include("LossCalculation.jl")
 end
 
 module Simulator
-export build_optimized_constellation, simulate, Node, Link, NodeLabel, satellite, ground_station, GroundStation, Propagator
+export build_optimized_constellation
+export Node, Link, NodeLabel
+export GroundStation, Propagator
+export satellite, ground_station
+export simulate
 include("Simulator.jl")
+end
+
+module VisualizeMap
+export plot_gses, plot_gs_selections
+include("VisualizeMap.jl")
 end
 
 end
